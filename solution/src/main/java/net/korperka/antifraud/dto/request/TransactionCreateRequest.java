474f28ad7bad042a79f1db527b074bfdd488bee1
korperka <korperkaaa@gmail.com>
2026-01-23 18:17:58 +0600

    fix

--- solution/src/main/java/net/korperka/antifraud/dto/request/TransactionCreateRequest.java
+++ solution/src/main/java/net/korperka/antifraud/dto/request/TransactionCreateRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import net.korperka.antifraud.entity.TransactionLocation;
 import net.korperka.antifraud.enums.TransactionChannel;
 
 import java.time.LocalDateTime;
@@ -38,7 +37,7 @@ public class TransactionCreateRequest {
     private String deviceId;
 
     private TransactionChannel channel;
-    private TransactionLocation location;
+    private TransactionLocationDTO location;
 
     private Map<String, Object> metadata;
 }
