bddd34e5212f8f2a82d6d049c294602c34cff20c
korperka <korperkaaa@gmail.com>
2026-01-23 21:10:32 +0600

    unwrap

--- solution/src/main/java/net/korperka/antifraud/dto/response/TransactionResponse.java
+++ solution/src/main/java/net/korperka/antifraud/dto/response/TransactionResponse.java
@@ -1,5 +1,6 @@
 package net.korperka.antifraud.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.korperka.antifraud.entity.Transaction;
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class TransactionResponse {
+    @JsonUnwrapped
     private Transaction transaction;
     private List<FraudRuleEvaluationResult> ruleResults;
 }
